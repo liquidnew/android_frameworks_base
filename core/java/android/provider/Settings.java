02057b9625809788df2c00b2ad5d3a51fdb9bbf1
liquid0624 <rjbirnesser@gmail.com>
2014-03-03 07:07:10 -0500

    Frameworks: status bar notification count (1/2)
    credit: burnsra & rmcc (cm)
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    
    Conflicts:
    	packages/SystemUI/res/values/dimens.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4369,6 +4369,12 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
 
         /**
+         * Show the pending notification counts as overlays on the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_NOTIFICATION_COUNT = "status_bar_notification_count";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

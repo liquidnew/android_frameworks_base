e332974caaed19840a2593e62b52391b62a9c9c1
Griffin Millender <griffinn.millender@gmail.com>
2014-03-08 12:10:47 -0300

    Frameworks: Ability to Enable/Disable Smart Wake covers
    Change-Id: I83d96696fefedc19fc3bce7265a4fcf2d4c894bc

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2675,6 +2675,12 @@ public final class Settings {
         public static final String LOCKSCREEN_GLOWPAD_TORCH = "lockscreen_glowpad_torch";
 
         /**
+         * Whether lid wakes the device
+         * @hide
+         */
+        public static final String LOCKSCREEN_LID_WAKE = "lockscreen_lid_wake";
+
+        /**
          * @deprecated Use {@link android.provider.Settings.Global#LOW_BATTERY_SOUND}
          * instead
          * @hide

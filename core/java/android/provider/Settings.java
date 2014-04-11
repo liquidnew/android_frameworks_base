c818fb2524dcc702b754debe04a185b193b4025a
Lars Greiss <kufikugel@googlemail.com>
2014-04-11 17:18:54 +0200

    Frameworks: SlimRecents: user configurable default expanded mode (1/2)
    A lot requests to customize the default behaviour
    
    auto = like it is now....dependend on the overlay the first tasks are expanded other collapsed
    always = always all expanded
    never = always all collapsed
    
    Of course still user changes directly in the recent panel counts more then the default settings
    behaviour
    
    PS
    - correct code comment to new behaviour
    PS
    - fix typo
    
    rebase
    
    Change-Id: I468be296de3c50d0036d2805c0d0cb8e54248e57

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4553,6 +4553,14 @@ public final class Settings {
         public static final String ON_THE_GO_SERVICE_RESTART = "on_the_go_service_restart";
 
         /**
+         * Recent panel expanded mode (auto = 0, always = 1, never = 2).
+         * default = 0.
+         *
+         * @hide
+         */
+        public static final String RECENT_PANEL_EXPANDED_MODE = "recent_panel_expanded_mode";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
